87a7a8a35096d7110bf85f6b709158e340a80d15
chris <38746057@qq.com>
2019-11-25 23:23:11 +0800

    移除rancher pipeline 示例

--- rancher/rancher pipeline samples/pipeline-example-php-master/resources/views/service-container.blade.php
+++
@@ -1,7 +0,0 @@
-@inject('converter', 'App\Test\StringConverter')
-
-@extends('layouts.scaffold')
-
-@section('main')
-    <p class="converted">{{ $converter->convert('String To Convert') }}</p>
-@stop
--- rancher/rancher pipeline samples/pipeline-example-php-master/tests/functional/UploadedFilesCept.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$I = new FunctionalTester($scenario);
-$I->wantTo('upload a file');
-$I->amOnPage(''); // Necessary to prevent LogicException with message "The page history is empty".
-$I->sendPOST('upload', [], ['file' => codecept_data_dir('logo.jpg')]);
-$I->see('Success');
